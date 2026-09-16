506d9081c609f2c23827dd443677080e3a769550
Shubham Bharti <shubhambharti336@gmail.com>
2026-09-16 19:55:18 +0530

    Update server.py

--- server.py
+++ server.py
@@ -4,10 +4,9 @@ from aurelius.server import mcp
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", "8000"))
+    mcp.settings.host = "0.0.0.0"
+    mcp.settings.port = int(os.environ.get("PORT", "8000"))
 
     mcp.run(
         transport="streamable-http",
-        host="0.0.0.0",
-        port=port,
     )
