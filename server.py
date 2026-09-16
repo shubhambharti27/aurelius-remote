a9ce4383187d9befeafb3fa28c34a18c82ef67f2
Shubham Bharti <shubhambharti336@gmail.com>
2026-09-16 19:42:14 +0530

    Update server.py

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", "8000"))
 
     mcp.run(
-        transport="http",
+        transport="streamable-http",
         host="0.0.0.0",
         port=port,
     )
